9492986433685bf1e8f42deb156a437ce7a08ab1
snyh <snyh@snyh.org>
2013-09-23 16:53:45 +0800

    create dui_test desktop

--- Source/dui/go.go
+++ Source/dui/go.go
@@ -9,7 +9,10 @@ import "io/ioutil"
 func main() {
     C.d_init()
     f := C.d_frame_new(300, 300)
-    content, _ := ioutil.ReadFile("test.htm")
+    content, _ := ioutil.ReadFile("/dev/shm/dui/Source/dui/test.htm")
     C.d_frame_load_content(f, C.CString(string(content)))
+
+    /*e := d_frame_get_element(C.CString("snyh"))*/
+
     C.d_main()
 }
