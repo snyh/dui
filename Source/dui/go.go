ca672a5b12c6d70f931cfe5952a81956ee48320d
snyh <snyh@snyh.org>
2013-09-25 23:51:08 +0800

    refine go.go

--- Source/dui/go.go
+++ Source/dui/go.go
@@ -7,7 +7,7 @@ import "io/ioutil"
 func main() {
     dui.Init()
 
-    f := dui.NewFrame(300, 300)
+    f := dui.NewFrame(350, 400)
     content, _ := ioutil.ReadFile("/dev/shm/dui/Source/dui/test.htm")
     f.LoadContent(string(content))
 
@@ -16,12 +16,11 @@ func main() {
 
     var flag bool
     f.Ele("button").SignalConnect("click", func() {
+        flag = !flag
         if flag {
             e.SetContent("one two three")
-            flag = false
         } else {
             e.SetContent("three two one")
-            flag = true
         }
     })
 
